a681425a4ef19c492c511f5fe016d7b1d4ee9da0
kcponline <kcponline@Admins-MacBook-Pro.local>
2016-08-02 22:40:17 -0700

    commit 02 - html snippets and js jq check

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+	// pseudo 01: check vjs link
+	document.write("js link check - to be comment out later");
+
+	// pseudo 01: check jq link
+	$("#jq-test").html("jq link check - to be comment out later");
