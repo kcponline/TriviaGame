5d18d25e4dbc8336a069820100f05aa8159dc25c
kcponline <kcponline@Admins-MacBook-Pro.local>
2016-08-03 01:22:13 -0700

    commit 04 - add questions and choices to hmtl

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,22 @@
 	// pseudo 01: check vjs link
-	document.write("js link check - to be comment out later");
+	// document.write("js link check - to be comment out later");
 
 	// pseudo 01: check jq link
-	$("#jq-test").html("jq link check - to be comment out later");
+	// $("#jq-test").html("jq link check - to be comment out later");
+
+function gameHide() {
+	$("#game-play-container").hide();
+}
+
+gameHide();
+
+
+function gameStart() {
+	$("#game-play-container").show();
+	// $(document).append("<p id="question1">What was the first full length CGI movie?</p>");
+	// $(document).append("<form><input type="radio" name="select1" value="a1" checked> A Bug's Life<input type="select1" name="" value="a2"> Monsters Inc.<input type="radio" name="select1" value="a3"> Toy Story</form><input type="radio" name="select1" value="a4"> The Lion King</form>");
+};
+
+function gameStop(argument) {
+	// body...
+}
